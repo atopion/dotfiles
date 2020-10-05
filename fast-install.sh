34c111440e4227842c0e4d4ae9c36cf7c05dda80
atopion <atopion@outlook.com>
2020-10-05 22:33:01 +0200

    Added fast-install.sh

--- fast-install.sh
+++ fast-install.sh
@@ -6,27 +6,8 @@ set -e
 # Download main repository
 git clone https://github.com/atopion/dotfiles.git ~/
 
+# Enter directory
+cd ~/dotfiles
 
-### VIM
-
-cp -r ~/dotfiles/.vim/ ~/.vim/
-
-# Download and install AYU-THEME
-git clone https://github.com/ayu-theme/ayu-vim.git ~/.vim/pack/plugins/start/ayu
-
-# Download and install NERDTree
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
-vim -u NONE -c "helptags ~/.vim/pack/plugins/start/nerdtree/doc" -c q
-
-# Download and install Lightline
-git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
-
-# Install .vimrc
-cp ~/dotfiles/.vimrc ~/.vimrc
-
-
-### TMUX
-
-# Install .tmux.conf
-cp ~/dotfiles/.tmux.conf ~/.tmux.conf
-
+# Execute install
+./install.sh
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Get path of this script
+SCRIPT=$(dirname "$(readlink -f "$0")")
+
+### VIM
+
+cp -r $SCRIPT/.vim/ ~/.vim/
+
+# Download and install AYU-THEME
+git clone https://github.com/ayu-theme/ayu-vim.git ~/.vim/pack/plugins/start/ayu
+
+# Download and install NERDTree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/plugins/start/nerdtree/doc" -c q
+
+# Download and install Lightline
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+
+# Install .vimrc
+cp $SCRIPT/.vimrc ~/.vimrc
+
+
+### TMUX
+
+# Install .tmux.conf
+cp $SCRIPT/.tmux.conf ~/.tmux.conf
+
